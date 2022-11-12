fe3c986d9c0eaefa168967d7c434a4c16751dccc
Davide Filosa <davideclaypool@gmail.com!>
2022-11-12 15:05:38 +0100

    Fixed bugs

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import plotly.express as px
 df = pd.read_csv('happy.csv')
 
 options = [item.replace('_', ' ').title() for item in df.iloc[:, 2:].columns]
-countries = df['country'].unique()
 st.header('In Search for Happiness')
-st.selectbox('Select a country', options=countries, key='country')
 st.selectbox('Select the data for the Y-axis', options=options, key='xaxis')
 xaxis = st.session_state['xaxis']
+
 xaxis_columns = xaxis.lower().replace(' ', '_')
 st.subheader(f'Happiness vs {xaxis}')
 y = df['happiness']
